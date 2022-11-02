eb2a2fdcc82ab7a95f74fb25f03c77ceec2254b0
eladshachar <elad.shachar@gmail.com>
2022-11-02 14:27:34 +0200

    basic exercise

---
+++ week-11/react-intro/playground/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    let text = 'dynamically'
+    ﻿return (
+      <h1>Going to display some text...{text}</h1>
+    );
+    }
+  }
+
+export default App;
